dcd4d5c75daa42789942818b9ab22a640fcaf3ce
Gabriel <leonismogabriel@gmail.com>
2024-11-16 22:47:57 -0300

    add post, put and delete to UsersController.cs

--- WebApplication2/Controllers/UsersController.cs
+++ WebApplication2/Controllers/UsersController.cs
@@ -33,4 +33,48 @@ public class UsersController(AppDbContext context) : ControllerBase
         }
         return user;
     }
+
+    [HttpPost]
+    public async Task<ActionResult<Users>> PutUsers(Users user)
+    {
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetUsers), new { id = user.Id }, user);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Users>> PutUsers(int id, Users user)
+    {
+        if (id != user.Id)
+        {
+            return BadRequest();
+        }
+        context.Users.Update(user);
+        try
+        {
+            await context.SaveChangesAsync();
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<Users>> DeleteUsers(int id)
+    {
+        if (!context.Users.Any())
+        {
+            return NotFound();
+        }
+        var user = await context.Users.FindAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        context.Users.Remove(user);
+        await context.SaveChangesAsync();
+        return NoContent();
+    }
 }
